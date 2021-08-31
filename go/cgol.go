c30d5a55b05e3be296166a956423e928ef51fdbb
Salvydas Lukosius <sall@w-ss.io>
2021-08-31 08:35:08 +0100

    renamed:    cgol/README.md -> README.md 	modified:   cgol.go

--- go/cgol.go
+++ go/cgol.go
@@ -104,10 +104,11 @@ func (l *Life) String() string {
 }
 
 func main() {
-	l := NewLife(40, 15)
-	for i := 0; i < 300; i++ {
+	l := NewLife(120, 30)
+	for i := 0; i < 1200; i++ {
 		l.Step()
 		fmt.Print("\x0c", l) // Clear screen and print field.
-		time.Sleep(time.Second / 30)
+		time.Sleep(time.Second / 5)
 	}
 }
+
